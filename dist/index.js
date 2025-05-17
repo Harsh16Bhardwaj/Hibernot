317d49c60625fcbc0e5a1b8e69bc806ce8ffbf1c
Harsh Bhardwaj <bhardwajharshmait23@gmail.com>
2025-05-17 20:10:21 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const hello = () => {
+    console.log("Hello from hibernot!");
+};
+exports.hello = hello;
+(0, exports.hello)();
